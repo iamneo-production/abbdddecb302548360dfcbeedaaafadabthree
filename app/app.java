83c6ec7eb8dbbed7b624ac5d8309a64a454c76be
Iamneo <techadmin@iamneo.ai>
2023-06-20 07:47:29 +0000

    Backup Commit

--- app/app.java
+++ app/app.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class app {
     public static void main(String[] args){
-        
+        System.out.print("gopi the ceo and play boy");
     }
-    System.out.print("gopi the ceo and play boy");
 }
